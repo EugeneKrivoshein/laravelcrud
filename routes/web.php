5864bb3ab63a51e9eec86529f5389ab9dab0c53b
Eugene <93378646+EugeneKrivoshein@users.noreply.github.com>
2023-01-07 17:46:13 +0300

    controller and router

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'users');
+Route::resource('users', UsersController::class);
